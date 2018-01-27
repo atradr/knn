557444aa9b23fec1d6d9ec8aa97fa16f58593731
Austin Rogers <austintradr@gmail.com>
2018-01-27 03:43:06 -0500

    First commit

---
+++ knn.py
@@ -0,0 +1,47 @@
+# Well, it works.
+
+import math
+
+import numpy as np
+
+
+def calculate_distance(test_row, train_row):
+    """ Returns the distance between two vectors.
+    """
+    element_sums = 0
+    # The -1 deals with the class name being last element.
+    # Need to change that.
+    for i in range(len(test_row) - 1):
+        element_sums += (test_row[i] - train_row[i])**2
+    return math.sqrt(element_sums)
+
+
+def get_nearest_class(test_row, train):
+    distance = calculate_distance(test_row, train[0])
+    nearest = train[0][4]
+    for row in train:
+        cur_dist = calculate_distance(test_row, row)
+        if cur_dist < distance:
+            distance = cur_dist
+            nearest = row[4]
+    return nearest
+
+
+def main():
+    data = np.genfromtxt('iris.data.txt', delimiter=',', dtype=None)
+
+    np.random.shuffle(data)
+
+    train = data[0:125]
+    test = data[125:]
+
+    num_correct = 0
+    for row in test:
+        nearest = get_nearest_class(row, train)
+        if row[4] == nearest:
+            num_correct += 1
+    print(num_correct)
+
+
+if __name__ == '__main__':
+    main()
